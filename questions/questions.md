03a21ca598ae57596b9e80f83eb3fa6cd16c7b4b
Nicholas Nguyen <nick.nguyen@contentstack.com>
2021-09-06 18:55:39 -0700

    Initial

--- questions/questions.md
+++ questions/questions.md
@@ -8,6 +8,20 @@ I'm also struggling with understanding what types of metrics would be useful to 
 
 Cheers, George
 
+*Question 1 Answer*:
+
+Hey George,
+
+Hope you're having a great week so far, below I've attached definitons of each concept, as well as links to our internal documentation that go a little more in depth.
+Records (https://www.algolia.com/doc/faq/basics/what-is-a-record/) - A record is an object you add to an index and want to search for. In short, it is a piece of data, whether a product, documentation, or other searchable thing you want your users to be able to find. 
+Index/Indexing (https://www.algolia.com/doc/api-client/methods/indexing/) - An index is the place where the data used by a search engine is stored, and indexing would be manipulating (adding, deletings, updating) said data into these indexes.
+Custom Ranking ("https://www.algolia.com/doc/guides/managing-results/must-do/custom-ranking/#custom-ranking") - We have our standard rankings that include geo-location, proximity, typos, etc., but custom rankings add in new properties to suggest in an order. In the JSON payload for the data, you can include any attributes you would like, such as number of words, popularity scoring, dates, and more. These will help filter results to users to data that is even more relevant to their search.
+
+Feel free to reach out to me if you have more questions, and more than happy enough to jump on a quick call and give you a quick overview.
+
+Cheers,
+Nick
+
 --
 
 *Question 2*: Hello,
@@ -16,6 +30,16 @@ Sorry to give you the kind of feedback that I know you do not want to hear, but 
 
 Regards, Matt
 
+
+*Questions 2 Answer*
+
+Hey Matt,
+
+Any feedback is good feedback, no need to apologize. I am sorry that the new user experience has not been treating you well. I can take this back into our product team and see what they think, or see if we have other customers experiencing the same issue, and their workaround/feedback they have sent in. Thank you for bringing this to our attention.
+
+Cheers,
+Nick
+
 --
 
 *Question 3*: Hi,
@@ -23,3 +47,12 @@ Regards, Matt
 I'm looking to integrate Algolia in my website. Will this be a lot of development work for me? What's the high level process look like?
 
 Regards, Leo
+
+*Question 3 Answer*:
+
+Hey Leo,
+
+We have many out of the box integrations in our documentation that should allow you to be all set up quite fast. It will not take you too much development work to get a simple search menu working, I think a good place to start would be in our Getting Started (https://www.algolia.com/doc/ui-libraries/autocomplete/introduction/getting-started/) documentation. As for a high level process, you would need to first import your formatted JSON data into Algolia. After your data is imported, a basic implementation would be a few snippets of JS code that tie into a front end component that will render the results you imported into Algolia. If you need any specific details on implementation when you start, feel free to email me and we can go over it together.
+
+Cheers,
+Nick
--- src/app.js
+++ src/app.js
@@ -1,27 +1,89 @@
-import Autocomplete from './components/autocomplete';
+import { autocomplete, getAlgoliaResults, getAlgoliaFacets  } from '@algolia/autocomplete-js';
+import { createQuerySuggestionsPlugin } from '@algolia/autocomplete-plugin-query-suggestions';
+import algoliasearch from 'algoliasearch';
+import {h, Fragment} from 'preact';
+import ProductItem from './components/ProductItem'
 
-class SpencerAndWilliamsSearch {
-  constructor() {
-    this._initSearch();
-    this._registerEvents();
+import '@algolia/autocomplete-theme-classic';
+
+const appId = 'K6C68ZR6FA';
+const apiKey = 'd76ff598c3dff31788b0ee20edcebfb9';
+const searchClient = algoliasearch(appId, apiKey);
+
+const querySuggestionsPlugin = createQuerySuggestionsPlugin({
+  searchClient,
+  indexName: 'search_query_suggestions',
+  getSearchParams() {
+    return {
+      hitsPerPage: 5,
+    };
+  },
+});
+
+autocomplete({
+  container: '#autocomplete', //Define where your search box goes in the DOM
+  placeholder: 'Search for products', //Define placeholder text
+  openOnFocus: true, //If true, dropdown appears as soon as users focuses the input to display trending searches 
+// debug: true, //If true, keeps the panel open when inspecting elements in your browser DevTools
+  plugins: [querySuggestionsPlugin], //Add the Query Suggestions plugin
+  getSources({ query, state }) { 
+    if (!query) {
+      return [];
+    }
+
+    return [
+      {
+        sourceId: 'products',        
+        getItems() {
+          return getAlgoliaResults({
+            searchClient,
+            queries: [
+              {
+                indexName: 'search',
+                query,
+                params: {            
+                  hitsPerPage: 3,                        
+                  attributesToSnippet: ['name:10'],
+                  snippetEllipsisText: '…',
+                },
+              },
+            ],
+          });
+        },
+        templates: {
+          header() {
+            return (
+              <Fragment>
+                <span className="aa-SourceHeaderTitle">Products</span>
+                <div className="aa-SourceHeaderLine" />
+              </Fragment>
+            );
+          },
+          item({ item, components }) {
+            return (
+                <ProductItem
+                    hit={item}
+                    components={components}
+              />
+            );
+          },
+          noResults() {
+            return 'No products for this query.';
+          },
+        },
+      },
+    ];
   }
+});
+  
 
-  _initSearch() {
-    this.autocompleteDropdown = new Autocomplete();
-  }
+const domSelector = document.querySelector('.autocomplete');
+const searchbox = document.querySelector('#autocomplete input');
 
-  _registerEvents() {
-    const autocomplete = document.querySelector('.autocomplete');
-    const searchbox = document.querySelector('#searchbox input');
+searchbox.addEventListener('click', () => {
+    domSelector.style.display = 'block';
+});
 
-    searchbox.addEventListener('click', () => {
-      autocomplete.style.display = 'block';
-    });
-
-    searchbox.addEventListener('blur', () => {
-      autocomplete.style.display = 'none';
-    });
-  }
-}
-
-const app = new SpencerAndWilliamsSearch();
+searchbox.addEventListener('blur', () => {
+    domSelector.style.display = 'none';
+});
